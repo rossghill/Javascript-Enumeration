e45cc532a536633828f43ef8b6ebf4b4f9985bec
Ross Hill <rosshill88@gmail.com>
2018-02-14 17:51:06 +0000

    1 test passing

---
+++ array_tasks.js
@@ -0,0 +1,39 @@
+const arrayTasks = {
+
+	concat: function (arr1, arr2) {
+    return arr1.concat(arr2);
+	},
+
+	// insertAt: function (arr, itemToAdd, index) {
+
+	// },
+
+	// square: function (arr) {
+
+	// },
+
+	// sum: function (arr) {
+
+	// },
+
+	// removeAndClone: function (arr, valueToRemove) {
+
+	// },
+
+	// findIndexesOf: function (arr, itemToFind) {
+
+	// },
+
+	// sumOfAllEvenNumbersSquared: function (arr) {
+
+	// }
+
+	// ----------- EXTENSION ------------
+
+	// findDuplicates: function (arr) {
+
+	// },
+
+}
+
+module.exports = arrayTasks
